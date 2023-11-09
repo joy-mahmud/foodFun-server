c530d320ef46193d6d707d8e9ccb85a319c3632e
joy-mahmud <joymahmudiucse@gmail.com>
2023-11-09 19:59:40 +0600

    jwt token generation api completed

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 5000
 
 //middleware
 app.use(cors({
-//   origin:['http://localhost:5173'],
-//   credentials:true
+  origin:['http://localhost:5173'],
+  credentials:true
 }))
 app.use(express.json())
 app.use(cookieParser())
@@ -28,6 +28,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyToken = async (req,res,next)=>{
+    const token = req.cookies?.token
+    if(!token){
+      return res.send({message:'not authorized'})
+    }
+    jwt.verify(token,process.env.ACCESSS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){
+        return res.status(401).send({message:'Unauthorized'})
+      }
+      console.log(decoded)
+      req.user = decoded
+      next();
+    })
+  }
+
 
 async function run() {
   try {
@@ -37,15 +52,30 @@ async function run() {
     const orderCollection = client.db('foodFun').collection('orderCollection')
    
     app.post('/jwt', async(req,res)=>{
-      const user = req.body
-      const token = jwt.sign(user,process.env.ACCESSS_TOKEN_SECRET, { expiresIn: '1h' }); 
-      res
-      .send({success:true})
-    })
+        const user = req.body
+        const token = jwt.sign(user,process.env.ACCESSS_TOKEN_SECRET, { expiresIn: '1h' }); 
+        res
+        .cookie('token',token,{
+          httpOnly:true,
+          secure:false,
+          
+         
+        })
+        .send({success:true})
+      })
+  
+      app.post('/logout', async(req,res)=>{
+        const user = req.body
+        console.log('logging out user',user)
+        res.clearCookie('token',{maxAge:0}).send({success:true})
+      })
 
     //get request to get all foods
     app.get('/allfoods', async(req,res)=>{
-        const cursor = foodCollection.find()
+        const page = parseInt(req.query.page)
+        const size =parseInt(req.query.size)
+       
+        const cursor = foodCollection.find().skip(page*size).limit(size)
         const result = await cursor.toArray()
         res.send(result)
     })
@@ -147,6 +177,12 @@ async function run() {
         const result = await orderCollection.deleteOne(query)
         res.send(result)
         
+    })
+
+    //pagination api
+    app.get('/foodcount', async(req,res)=>{
+        const count = await foodCollection.estimatedDocumentCount()
+        res.send({count})
     })
 
    
